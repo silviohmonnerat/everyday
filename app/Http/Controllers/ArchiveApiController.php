3b78f01baa261c3a42562197fc393cb33599f3a8
Silvio Monnerat <silviomonnerat@gmail.com>
2022-03-09 19:14:10 -0300

    ✅ api search

--- app/Http/Controllers/ArchiveApiController.php
+++ app/Http/Controllers/ArchiveApiController.php
@@ -32,4 +32,21 @@ class ArchiveApiController extends Controller
 
         return response($archive, 200);
     }
+
+    /**
+     * Search a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function search($search)
+    {
+        $archives = Archive::search($search)->get();
+
+        if (count($archives)) {
+         return Response($archives, 200);
+        }
+
+        return response(['message' => 'No Archive not found'], 404);
+    }
 }
